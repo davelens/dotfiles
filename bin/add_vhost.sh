eaf24ce3d4603fbd765e06eff867359ddbbd8122
Dave Lens <dave@netlash.com>
2012-02-28 15:11:21 +0100

    add shellscript with matching alias to add a vhost /etc/apache2/other

---
+++ bin/add_vhost.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+project=$1
+docroot=$2
+
+if [[ $project == "" ]] || [[ $docroot == "" ]] || [[ `whoami` != 'root' ]];
+then
+	echo "usage: sudo ./add_vhost.sh <project> <document_root>"
+else
+	# create a separate .conf file for this vhost
+	echo -e "<VirtualHost *:80>\n\tServerName $project.dev\n\tDocumentRoot $docroot\n\n\t<Directory $docroot>\n\t\tOptions Includes FollowSymLinks\n\t\tAllowOverride All\n\t\tOrder allow,deny\n\t\tAllow from all\n\t</Directory>\n</VirtualHost>" >> /etc/apache2/other/$project.conf
+
+	# append the TLD to the hosts file
+	echo -e "127.0.0.1\t$project.dev" >> /etc/hosts
+
+	# restart apache
+	apachectl restart
+fi
+
