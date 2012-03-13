4e3ec154a0dd11d7e8a119d8551d7e576c95603b
Dave Lens <dave.lens@wijs.be>
2012-03-13 22:41:36 +0100

    split up mac/linux specific commands and aliases so they don't get loaded in the wrong place.

--- bash_profile
+++ bash_profile
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Set a global variable representing the OS we're on.
+# This way I can filter out dotfiles content for specific OSes.
+# Mac OS X = Darwin
+# Ubuntu, Debian, Mint,...  Linux
+OS=`uname -s`
+
 # Source the files in the bash folder
 for file in ~/.bash/{shell,commands,prompt,aliases,private}; do
 	[ -r "$file" ] && source "$file";
 done;
 unset file;
 
-
+if [[ $OS == 'Darwin' ]];
+then
+	source ~/.bash/osx
+fi
--- bin/add_vhost.sh
+++ bin/add_vhost.sh
@@ -7,8 +7,17 @@ if [[ $project == "" ]] || [[ $docroot == "" ]] || [[ `whoami` != 'root' ]];
 then
 	echo "usage: sudo ./add_vhost.sh <project> <document_root>"
 else
+	# Apache uses /etc/apache2/sites-enabled as vhost folder, Mac uses /etc/apache2/other
+	if [[ $OS == 'Darwin' ]];
+	then
+		vhost_folder='other'
+	else
+		vhost_folder='sites-enabled'
+	fi
+
+
 	# create a separate .conf file for this vhost
-	echo -e "<VirtualHost *:80>\n\tServerName $project.dev\n\tDocumentRoot $docroot\n\n\t<Directory $docroot>\n\t\tOptions Includes FollowSymLinks\n\t\tAllowOverride All\n\t\tOrder allow,deny\n\t\tAllow from all\n\t</Directory>\n</VirtualHost>" >> /etc/apache2/other/$project.conf
+	echo -e "<VirtualHost *:80>\n\tServerName $project.dev\n\tDocumentRoot $docroot\n\n\t<Directory $docroot>\n\t\tOptions Includes FollowSymLinks\n\t\tAllowOverride All\n\t\tOrder allow,deny\n\t\tAllow from all\n\t</Directory>\n</VirtualHost>" >> /etc/apache2/$vhost_folder/$project.conf
 
 	# append the TLD to the hosts file
 	echo -e "127.0.0.1 $project.dev" >> /etc/hosts
