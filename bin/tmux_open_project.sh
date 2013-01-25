bc99d6c690929769279658fadcff2819d65b69d2
davelens <github@davelens.be>
2013-01-25 08:32:55 +0100

    add clear statements to window commands upon opening a new session

--- bin/tmux_open_project.sh
+++ bin/tmux_open_project.sh
@@ -51,18 +51,18 @@ function open_tmux_session()
 	# editor window
 	tmux new-session -s $project -n editor -d
 	tmux send-keys -t $project "cd $path/$project" C-m
-	tmux send-keys -t $project "vim" C-m
+	tmux send-keys -t $project "clear && vim" C-m
 	# database window
 	tmux new-window -n database -t $project
 	tmux send-keys -t $project:2 "cd $path/$project" C-m
-	tmux send-keys -t $project:2 "./dbshell" C-m
+	tmux send-keys -t $project:2 "clear && ./dbshell" C-m
 	tmux split-window -v -p 15 -t database
-	tmux send-keys -t $project:2 "php -a" C-m
+	tmux send-keys -t $project:2 "clear && php -a" C-m
 	tmux select-pane -t $project:2 -U
 	# shell window
 	tmux new-window -n shell -t $project
 	tmux send-keys -t $project:3 "cd $path/$project" C-m
-	tmux send-keys -t $project:3 "git st" C-m
+	tmux send-keys -t $project:3 "clear && git st" C-m
 	# select the editor and attach to the session
 	tmux select-window -t $project:1
 }
