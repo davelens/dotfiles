eedd01c3cb8d77b796ffe22b310da2ab263397d2
davelens <github@davelens.be>
2013-10-21 14:16:17 +0200

    add command to close an active "project"

---
+++ bin/tmux_close_project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+company=$1
+project=$2
+
+if [ "$company" == "" ]; then
+	echo "$(tput setaf 11)What project do you want to close?$(tput sgr0)"
+	echo -n "$(tput sgr0)$(tput bold)blimp/$(tput sgr0)"
+	company='blimp'
+	read project
+fi
+
+if [ "$project" == "" ]; then
+	company='blimp'
+	project=$1
+fi
+
+tmux kill-session -t $company/$project
