36e2d72428d65729abe489c8c686dbff2fb3bd23
davelens <github@davelens.be>
2015-03-20 10:05:48 +0100

    make sure ~/.guard.rb looks for Guard in the right namespace

--- guard.rb
+++ guard.rb
@@ -1,4 +1,4 @@
-clearing :on if Guard::VERSION.to_s.include?('2.12')
+clearing :on if ::Guard::VERSION.to_s.include?('2.12')
 notification :tmux,
   display_message: true,
   color_location: %w(pane-border-fg pane-active-border-fg)
