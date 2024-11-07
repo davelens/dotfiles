148e766c84e4fd127bee3f1e304740c506d73260
davelens <github@davelens.be>
2024-11-07 23:06:32 +0100

    Rename bin/dave to bin/utility, and bin/dave-scripts to bin/utilities.

--- bash/completions/utility_completion.bash
+++ bash/completions/utility_completion.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-_dave_completions() {
+_utility_completions() {
   local cur prev subfolders scripts
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     # Define the root directory for the scripts
-    local dave_root="${HOME}/.dotfiles/bin/dave-scripts"
+    local utilities_root="${HOME}/.dotfiles/bin/utilities"
 
-    # If this is the first argument after "dave", we list the subfolders
+    # If this is the first argument after "utility", we list the subfolders
     if [[ $COMP_CWORD -eq 1 ]]; then
-      # List all subdirectories within ~/.bin/dave-scripts
-      subfolders=$(find -L "$dave_root" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+      # List all subdirectories within ~/.bin/utilities
+      subfolders=$(find -L "$utilities_root" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
       COMPREPLY=( $(compgen -W "$subfolders" -- "$cur") )
       return 0
     fi
 
     # If the first argument is already a valid subfolder, list scripts in that folder
-    local folder_path="$dave_root/${COMP_WORDS[1]}"
+    local folder_path="$utilities_root/${COMP_WORDS[1]}"
     if [[ -d "$folder_path" && $COMP_CWORD -eq 2 ]]; then
       # List all files within the specified subfolder
       scripts=$(find -L "$folder_path" -type f -exec basename {} \;)
@@ -30,5 +30,5 @@ _dave_completions() {
     return 0
   }
 
-# Register the completion function for the "dave" command
-complete -F _dave_completions dave
+# Register the completion function for the `utility` command and its alias `u`.
+complete -F _utility_completions u utility
