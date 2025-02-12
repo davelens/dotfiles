1574971e6cfa2d5f25d2effb44304af7fc204467
davelens <github@davelens.be>
2025-02-12 17:20:15 +0100

    Disable the passkey prompting "by default" for now.

--- bash/shell.sh
+++ bash/shell.sh
@@ -255,16 +255,34 @@ if [[ ! -f "$DOTFILES_SALT_PATH" ]]; then
   unset BW_SESSION
 fi
 
-if [[ -z $TMUX ]]; then
-  salt=$(salt current)
+#
+# Outside of tmux, we ask for the salt passkey once and store it in an ENV var.
+# Because I'm in tmux 24/7 I have access to DOTFILES_SALT in all my panes and 
+# windows.
+#
+# This way we don't need to enter our Bitwarden master password beyond the
+# first time, ever.
+#
+# TODO: This is functional but I've disabled it for now.
+#
+# The issue is that ENV vars (ie. DOTFILES_SALT) don't persist outside of 
+# subshells.
+#
+# I might have to ask for the passkey to the salt every single time. It won't 
+# defeat the purpose of having a salt
+#
+# `utility misc screenshot` would ask for a passkey every time, for instance.
+#
+#if [[ -z $TMUX ]]; then
+  #salt=$(salt current)
 
-  if [[ $? -eq 0 ]]; then
-    export DOTFILES_SALT="$salt"
+  #if [[ $? -eq 0 ]]; then
+    #export DOTFILES_SALT="$salt"
 
-    if [[ -z $BW_SESSION ]]; then
-      export BW_SESSION="$(utility misc bitwarden unlock)"
-    fi
-  else
-    print-status -i error "Encrypted salt not ready; possibly wrong passkey."
-  fi
-fi
+    #if [[ -z $BW_SESSION ]]; then
+      #export BW_SESSION="$(utility misc bitwarden unlock)"
+    #fi
+  #else
+    #print-status -i error "Encrypted salt not ready; possibly wrong passkey."
+  #fi
+#fi
