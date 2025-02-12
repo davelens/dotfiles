84044d4e7612c805b0a2eedd010de367ca26b8bd
davelens <github@davelens.be>
2025-02-12 23:39:07 +0100

    Extract the defaults from the wezterm configuration, and conditionally load in macos or windows settings.

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -3,24 +3,19 @@
 -- The config below is as close as I can get to that.
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local platforms = {
+  macos = wezterm.target_triple:find("apple"),
+  windows = wezterm.target_triple:find("windows")
+}
 
--- TODO: Two issues for me remain:
--- - [ ] Hack Nerd Font's glyphs are (too) small. How to scale them?
--- - [ ] It's very minor and nitpicking, but wezterm has a couple of pixels of
---       border bottom I can't seem to shake. Find out why.
-
+-- Defaults!
 config.max_fps = 240 -- Fixes the (s)low default of 60, this feels snappier.
-config.native_macos_fullscreen_mode = false
 config.enable_tab_bar = false
--- This provides us the same behaviour as iTerm2's top border
--- obfuscating the little camera inlay on macbooks nowadays.
--- Obviously I won't need that on other machines, so this should be
--- macos only (ideally macbook only, but I'll settle).
+
 config.window_frame = {
   border_bottom_height = '0',
-  border_top_height = '.7cell',
-  border_top_color = 'black',
 }
+
 config.window_padding = {
   left = 10,
   right = 10,
@@ -30,14 +25,8 @@ config.window_padding = {
 
 -- Nord theme on Monaco with Hack has been my jam for a while now.
 config.color_scheme = 'nord'
-config.line_height = 1.03
 config.cell_width = 1.01
-config.font_size = 16.0
-config.font = wezterm.font_with_fallback {
-  { family = "Monaco", weight = "Regular" },
-  { family = "Hack Nerd Font" },
-  { family = "Apple Color Emoji" },
-} 
+config.font_size = 14.0
 
 config.keys = {
   -- Disable most keybinds associated with tab management, I don't use em.
@@ -59,23 +48,38 @@ config.keys = {
   { mods = 'SHIFT | SUPER', key = ']', action = wezterm.action.DisableDefaultAssignment }, -- ActivateTabRelative(1)
   -- Disable the wezterm search; I have more control with the bash default.
   { mods = 'SUPER', key = 'f', action = wezterm.action.DisableDefaultAssignment }, -- Search(CurrentSelectionOrEmptyString)
-  -- CMD/Win+Enter is how I've been triggering full screen for years.
-  { mods = 'ALT', key = 'Enter', action = wezterm.action.DisableDefaultAssignment }, -- ToggleFullScreen
-  { mods = 'SUPER', key = 'Enter', action = wezterm.action.ToggleFullScreen },
 }
 
--- Windows only settings
-if wezterm.target_triple:find("windows") then
-  config.prefer_egl = true
-  config.enable_wayland = false
-  config.default_domain = 'WSL:Arch'
-  config.default_prog = {
-    "wsl.exe", "-u", "davelens", "--", "bash", "-c", "cd ~ && exec bash"
-  }
+if platforms.macos then
+  config.native_macos_fullscreen_mode = false
 
   config.window_frame = {
-    border_bottom_height = '0',
-    border_top_height = '0',
+    border_top_height = '.7cell',
+    border_top_color = 'black',
+  }
+
+  config.line_height = 1.03
+  config.cell_width = 1.01
+  config.font_size = 16.0
+  config.font = wezterm.font_with_fallback {
+    { family = "Monaco", weight = "Regular" },
+    { family = "Hack Nerd Font" },
+    { family = "Apple Color Emoji" },
+  } 
+
+  -- CMD+Enter is how I've been triggering full screen for years.
+  config.keys = {
+    { mods = 'ALT', key = 'Enter', action = wezterm.action.ToggleFullScreen }, -- ToggleFullScreen
+    { mods = 'SUPER', key = 'Enter', action = wezterm.action.DisableDefaultAssignment },
+  }
+end
+
+-- Load platform specific configurations
+if platforms.windows then
+  config.prefer_egl = true
+  config.enable_wayland = false
+  config.default_prog = {
+    "wsl.exe", "-d", "Arch", "-u", "davelens", "--", "bash", "-c", "cd ~ && exec bash"
   }
 
   config.line_height = 1.0
@@ -84,11 +88,6 @@ if wezterm.target_triple:find("windows") then
   config.font = wezterm.font_with_fallback {
     { family = "MonegoLigatures Nerd Font", weight = "Regular" },
     { family = "Hack Nerd Font" },
-  }
-
-  config.keys = {
-    { mods = 'ALT', key = 'Enter', action = wezterm.action.ToggleFullScreen }, -- ToggleFullScreen
-    { mods = 'SUPER', key = 'Enter', action = wezterm.action.DisableDefaultAssignment },
   }
 end
 
