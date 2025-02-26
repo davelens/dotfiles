f1e6f798a6978b04002d3f0df8bbf17a8fc960fb
davelens <github@davelens.be>
2025-02-26 14:46:07 +0100

    tmux/quickstart: Add the first usecase of subcommand bash completion for bin/utilities commands.

--- bash/completions/utility_completion.bash
+++ bash/completions/utility_completion.bash
@@ -1,14 +1,20 @@
+# What is key to understanding completion functions is that they get run
+# every time you trigger a completion to occur.
 _utility_completions() {
-  local cur prev subcommand subcommand_completion_path subfolders scripts
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  local input prev subcommand subcommand_completion_path subfolders scripts
+  input="${COMP_WORDS[COMP_CWORD]}"
   subcommand="${COMP_WORDS[1]}"
-  subcommand_completion_path="$DOTFILES_PATH/bash/completions/${subcommand}_${COMP_WORDS[2]}.bash"
+  subcommand_completion_path="$DOTFILES_PATH/bash/completions/${subcommand}_${COMP_WORDS[2]}"
 
   # Define the root directory for the scripts
   local utilities_root="${DOTFILES_PATH}/bin/utilities"
 
+  # We autoload all bash/completions/*.bash files, so don't add the .bash
+  # extension to specific subcommands' completion files.
   if [[ -f $subcommand_completion_path ]]; then
+    # NOTE: Because we source the scripts here rather than having them as
+    # executables, any functions defined in these files will exist in the
+    # global namespace. Remember to unset those functions after use!
     source $subcommand_completion_path
     return 0
   fi
@@ -17,7 +23,7 @@ _utility_completions() {
   if [[ $COMP_CWORD -eq 1 ]]; then
     # List all subdirectories within ~/.bin/utilities
     subfolders=$(find -L "$utilities_root" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
-    COMPREPLY=( $(compgen -W "$subfolders" -- "$cur") )
+    COMPREPLY=( $(compgen -W "$subfolders" -- "$input") )
     return 0
   fi
 
@@ -26,7 +32,7 @@ _utility_completions() {
   if [[ -d "$folder_path" && $COMP_CWORD -eq 2 ]]; then
     # List all files within the specified subfolder
     scripts=$(find -L "$folder_path" -type f ! -name "_*" ! -name "*.sh" -exec basename {} \;)
-    COMPREPLY=( $(compgen -W "$scripts" -- "$cur") )
+    COMPREPLY=( $(compgen -W "$scripts" -- "$input") )
     return 0
   fi
 
