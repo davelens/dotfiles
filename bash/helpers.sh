34ee4415bc01542fbb8e54006079674f0241a696
davelens <github@davelens.be>
2025-03-02 18:08:55 +0100

    Get rid of the function exports; using BASH_ENV does that heavy lifting in a safer manner.

--- bash/helpers.sh
+++ bash/helpers.sh
@@ -8,7 +8,6 @@ fi
 function fn_exists { 
   declare -F "$1" > /dev/null
 }
-#export -f fn_exists
 
 function block_unless_sourced {
   if is_sourced; then
@@ -18,24 +17,20 @@ function block_unless_sourced {
 
   return 0
 }
-#export -f block_unless_sourced
 
 function check {
   [ -z "$1" ] && set -- 255
   echo "$(colorize "$1" "[")$(green ✓)$(colorize "$1" "]")"
 }
-#export -f check
 
 function cross {
   [ -z "$1" ] && set -- 255
   echo "$(colorize "$1" "[")$(red x)$(colorize "$1" "]")"
 }
-#export -f cross
 
 function colorize {
   echo "$(tput setaf "$1")$2$(tput sgr0)"
 }
-#export -f colorize
 
 function ensure_brew_dependency {
   for package in "$@"; do
@@ -56,7 +51,6 @@ function ensure_brew_dependency {
     fi
   done
 }
-#export -f ensure_brew_dependency
 
 # To help us centralize how errors look throughout our scripts.
 function error_handler {
@@ -66,39 +60,33 @@ function error_handler {
     exit $?
   fi
 }
-#export -f error_handler
 
-# Exports all ENV vars listed in a file. Loads ~/.env by default.
+# Exports all ENV vars listed in a file.
+# Loads ${DOTFILES_CONFIG_PATH}/env by default.
 function export_env_vars_from_file {
-  local env_file=${1:-~/.env}
-  # shellcheck source=/dev/null
-  [[ -f $env_file ]] && source "$env_file"
+  local env_file=${1:-"${DOTFILES_CONFIG_PATH}/env"}
+  [ -f $env_file ] && source "$env_file"
 }
-#export -f export_env_vars_from_file
 
 # Helps us hard stop our custom executables during fails.
 function fail {
   printf '%s\n' "$1" >&2 # Sends a message to stderr.
   exit "${2-1}" # Returns a code specified by $2 or 1 by default.
 }
-#export -f fail
 
 function green {
   colorize 2 "$1"
 }
-#export -f green
 
 function interrupt_handler {
   print_status -i error "Aborted."
   exit 1
 }
-#export -f interrupt_handler
 
 function is_sourced {
   local script="${BASH_SOURCE[1]}"
   [[ "$script" != "$0" ]]
 }
-#export -f is_sourced
 
 # Join an array by a given delimiter string
 function join_by {
@@ -109,7 +97,6 @@ function join_by {
     printf "%s" "$f" "${@/#/$d}"
   fi
 }
-#export -f join_by
 
 # Lowercase any string
 function lowercase {
@@ -119,13 +106,11 @@ function lowercase {
     cat - | tr "[:upper:]" "[:lower:]"
   fi
 }
-#export -f lowercase
 
 function pending {
   [ -z "$1" ] && set -- 255
   echo "$(colorize "$1" "[")$(yellow \~)$(colorize "$1" "]")"
 }
-#export -f pending
 
 # Find the process ID of a given command. Note that you can use regex as well.
 # 
@@ -135,12 +120,10 @@ function pending {
 function pid { 
   lsof -t -c "$@"
 }
-#export -f pid
 
 function red {
   colorize 1 "$1"
 }
-#export -f red
 
 # Examples:
 #
@@ -170,25 +153,21 @@ function repeat {
     for i in $(seq "${times:-1}"); do $commands; done
   fi
 }
-#export -f repeat
 
 function succeed {
   echo "$1" # Sends a message to stderr.
   exit 0
 }
-#export -f succeed
 
 # Because we all want to know how many times we actually typed "gti" instead 
 # of "git".
 function timesused {
   [ -f "$HISTFILE" ] && grep -c "^${1}" "$HISTFILE"
 }
-#export -f timesused
 
 function yellow {
   colorize 3 "$1"
 }
-#export -f yellow
 
 # This is an easy way to expose my bash scripting utilities without having to
 # prefix the full utility command.
