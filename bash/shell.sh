d52ef7d5b6179add18f8c46bbac9a587676c7ccf
davelens <github@davelens.be>
2025-03-02 17:31:27 +0100

    bash/shell: Disable macos keeping separate history per session in ~/.bash_sessions (and double HISTSIZE to 8192).

--- bash/shell.sh
+++ bash/shell.sh
@@ -172,8 +172,11 @@ shopt -s histreedit
 # Append to the history file rather than overwriting
 shopt -s histappend
 
+# Disable macos keeping separate history per session in ~/.bash_sessions.
+export SHELL_SESSION_HISTORY=0
+
 # Keep a reasonably long history.
-export HISTSIZE=4096
+export HISTSIZE=8192
 
 # Keep even more history lines inside the file, so we can still look up
 # previous commands without needlessly cluttering the current shell's history.
