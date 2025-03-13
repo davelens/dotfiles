8abcbe7ca47eff4131bf94c28b9881135caa7229
davelens <github@davelens.be>
2025-03-13 21:04:39 +0100

    Fix up and load the colors file before we declare our prompt.

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,38 +1,42 @@
 # Largely inspired by http://mywiki.wooledge.org/BashFAQ/037
 
 # This seems to be more consistent than \033[0m
-export NONE=$(tput sgr0)      # Unsets color to term's fg color.
-export EM=$(tput bold)        # Bold
-export EM=$(tput sitm)        # Start italic mode
-export NEM=$(tput ritm)       # Reset italic mode
-export UN=$(tput smul)        # Start underline mode
-export NUN=$(tput rmul)       # Reset underline mode
-export STR=$(printf '\e[9m')  # Start strikethrough mode
+NONE="$(tput sgr0)"      # Unsets color to term's fg color.
+B="$(tput bold)"         # Bold
+EM="$(tput sitm)"        # Start italic mode
+NEM="$(tput ritm)"       # Reset italic mode
+UN="$(tput smul)"        # Start underline mode
+NUN="$(tput rmul)"       # Reset underline mode
+STR="$(printf '\e[9m')"  # Start strikethrough mode
+export NONE EM NEM UN NUN STR
 
 # Background colors
-export BGK=$(tput setab 0)    # black
-export BGR=$(tput setab 1)    # red
-export BGG=$(tput setab 2)    # green
-export BGY=$(tput setab 3)    # yellow
-export BGB=$(tput setab 4)    # blue
-export BGM=$(tput setab 5)    # magenta
-export BGC=$(tput setab 6)    # cyan
-export BGW=$(tput setab 7)    # white
+BGK="$(tput setab 0)"    # black
+BGR="$(tput setab 1)"    # red
+BGG="$(tput setab 2)"    # green
+BGY="$(tput setab 3)"    # yellow
+BGB="$(tput setab 4)"    # blue
+BGM="$(tput setab 5)"    # magenta
+BGC="$(tput setab 6)"    # cyan
+BGW="$(tput setab 7)"    # white
+export BGK BGR BGG BGY BGB BGM BGC BGW
 
-export K=$(tput setaf 0)      # black
-export R=$(tput setaf 1)      # red
-export G=$(tput setaf 2)      # green
-export Y=$(tput setaf 3)      # yellow
-export B=$(tput setaf 4)      # blue
-export M=$(tput setaf 5)      # magenta
-export C=$(tput setaf 6)      # cyan
-export W=$(tput setaf 7)      # white
+K="$(tput setaf 0)"      # black
+R="$(tput setaf 1)"      # red
+G="$(tput setaf 2)"      # green
+Y="$(tput setaf 3)"      # yellow
+B="$(tput setaf 4)"      # blue
+M="$(tput setaf 5)"      # magenta
+C="$(tput setaf 6)"      # cyan
+W="$(tput setaf 7)"      # white
+export K R G Y B M C W
 
-export BK=${BOLD}${K}         # Bold black
-export BR=${BOLD}${R}         # Bold red
-export BG=${BOLD}${G}         # Bold green
-export BY=${BOLD}${Y}         # Bold yellow
-export BB=${BOLD}${B}         # Bold blue
-export BM=${BOLD}${M}         # Bold magenta
-export BC=${BOLD}${C}         # Bold cyan
-export BW=${BOLD}${W}         # Bold white
+BK=${BOLD}$K             # Bold black
+BR=${BOLD}${R}           # Bold red
+BG=${BOLD}${G}           # Bold green
+BY=${BOLD}${Y}           # Bold yellow
+BB=${BOLD}${B}           # Bold blue
+BM=${BOLD}${M}           # Bold magenta
+BC=${BOLD}${C}           # Bold cyan
+BW=${BOLD}${W}           # Bold white
+export BK BR BG BY BB BM BC BW
