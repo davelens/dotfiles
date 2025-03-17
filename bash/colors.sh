864291f3bd5c530e79ec14968ed8704a5f1898b9
davelens <github@davelens.be>
2025-03-17 17:07:13 +0100

    Redefine better prefixes for all colors, part 1.

--- bash/colors.sh
+++ bash/colors.sh
@@ -17,14 +17,25 @@ if [[ ! "$LS_COLORS" =~ "di=0;34:" ]]; then
 fi
 
 # This seems to be more consistent than \033[0m
-NONE="$(tput sgr0)"     # Unsets color to term's fg color.
-B="$(tput bold)"        # Bold
-EM="$(tput sitm)"       # Start italic mode
-NEM="$(tput ritm)"      # Reset italic mode
-UN="$(tput smul)"       # Start underline mode
-NUN="$(tput rmul)"      # Reset underline mode
-STR="$(printf '\e[9m')" # Start strikethrough mode
-export NONE EM NEM UN NUN STR
+CNONE="$(tput sgr0)"     # Unsets color to term's fg color.
+CBOLD="$(tput bold)"     # Bold
+CEM="$(tput sitm)"       # Start italic mode
+CNEM="$(tput ritm)"      # Reset italic mode
+CUN="$(tput smul)"       # Start underline mode
+CNUN="$(tput rmul)"      # Reset underline mode
+CSTR="$(printf '\e[9m')" # Start strikethrough mode
+export CNONE CBOLD CEM CNEM CUN CNUN CSTR
+
+# Foreground colors
+FGK="$(tput setaf 0)" # black
+FGR="$(tput setaf 1)" # red
+FGG="$(tput setaf 2)" # green
+FGY="$(tput setaf 3)" # yellow
+FGB="$(tput setaf 4)" # blue
+FGM="$(tput setaf 5)" # magenta
+FGC="$(tput setaf 6)" # cyan
+FGW="$(tput setaf 7)" # white
+export FGK FGR FGG FGY FGB FGM FGC FGW
 
 # Background colors
 BGK="$(tput setab 0)" # black
@@ -37,22 +48,12 @@ BGC="$(tput setab 6)" # cyan
 BGW="$(tput setab 7)" # white
 export BGK BGR BGG BGY BGB BGM BGC BGW
 
-K="$(tput setaf 0)" # black
-R="$(tput setaf 1)" # red
-G="$(tput setaf 2)" # green
-Y="$(tput setaf 3)" # yellow
-B="$(tput setaf 4)" # blue
-M="$(tput setaf 5)" # magenta
-C="$(tput setaf 6)" # cyan
-W="$(tput setaf 7)" # white
-export K R G Y B M C W
-
-BK=${BOLD}$K   # Bold black
-BR=${BOLD}${R} # Bold red
-BG=${BOLD}${G} # Bold green
-BY=${BOLD}${Y} # Bold yellow
-BB=${BOLD}${B} # Bold blue
-BM=${BOLD}${M} # Bold magenta
-BC=${BOLD}${C} # Bold cyan
-BW=${BOLD}${W} # Bold white
-export BK BR BG BY BB BM BC BW
+CBK=$CBOLD$K # Bold black
+CBR=$CBOLD$R # Bold red
+CBG=$CBOLD$G # Bold green
+CBY=$CBOLD$Y # Bold yellow
+CBB=$CBOLD$B # Bold blue
+CBM=$CBOLD$M # Bold magenta
+CBC=$CBOLD$C # Bold cyan
+CBW=$CBOLD$W # Bold white
+export CBK CBR CBG CBY CBB CBM CBC CBW
