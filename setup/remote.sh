33c3572d820102b7382536f50590bd386a82e463
davelens <github@davelens.be>
2025-03-18 16:13:44 +0100

    Fix up setup/remote.sh so reliable testing can happen.

--- setup/remote.sh
+++ setup/remote.sh
@@ -17,8 +17,11 @@ DOTFILES_FOLDER="dots"
 DOTFILES_STATE_HOME="$XDG_STATE_HOME/$DOTFILES_FOLDER"
 readonly DOTFILES_FOLDER DOTFILES_STATE_HOME
 
-[ ! -d "$DOTFILES_STATE_HOME" ] && mkdir -p "$DOTFILES_STATE_HOME/tmp"
+[ ! -d "$DOTFILES_STATE_HOME/tmp" ] && mkdir -p "$DOTFILES_STATE_HOME/tmp"
 
 curl -L -o "$DOTFILES_STATE_HOME/tmp/dotfiles.zip" https://github.com/davelens/dotfiles/archive/refs/heads/master.zip
 # TODO: Replace with extracting a tarball when we're starting with releases.
 unzip -o "$DOTFILES_STATE_HOME/tmp/dotfiles.zip" -d "$DOTFILES_STATE_HOME/tmp"
+
+rm -rf "$DOTFILES_STATE_HOME/tmp/dotfiles-master"
+rm -f "$DOTFILES_STATE_HOME/tmp/dotfiles.zip"
