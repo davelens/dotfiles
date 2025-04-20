30ce3a68e30a8c6abcd6b986cfdad1d434dd338c
davelens <github@davelens.be>
2025-04-20 23:27:07 +0200

    config/macos: Add sketchybar config.

---
+++ config/macos/sketchybar/plugins/current_space.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+update_space() {
+  SPACE_ID=$(echo "$INFO" | jq -r '."display-1"')
+
+  case $SPACE_ID in
+  1)
+    ICON=''
+    ICON_PADDING_LEFT=8
+    ICON_PADDING_RIGHT=7
+    ;;
+  *)
+    ICON=$SPACE_ID
+    ICON_PADDING_LEFT=9
+    ICON_PADDING_RIGHT=10
+    ;;
+  esac
+
+  sketchybar --set $NAME \
+    icon=$ICON \
+    icon.padding_left=$ICON_PADDING_LEFT \
+    icon.padding_right=$ICON_PADDING_RIGHT
+}
+
+case "$SENDER" in
+"mouse.clicked")
+  # Reload sketchybar
+  sketchybar --remove '/.*/'
+  source $HOME/.config/sketchybar/sketchybarrc
+  ;;
+*)
+  update_space
+  ;;
+esac
