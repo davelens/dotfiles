1fde9bc4ca6320ac68dd27526b21f3e0ff689192
davelens <github@davelens.be>
2025-04-29 12:32:06 +0200

    config/macos/sketchybar: I don't subscribe to barbarism, so switch around artist + track.

--- config/macos/sketchybar/plugins-laptop/spotify.sh
+++ config/macos/sketchybar/plugins-laptop/spotify.sh
@@ -44,7 +44,7 @@ update_track() {
         ARTIST="${ARTIST:0:$((MAX_LENGTH - TRACK_LENGTH - 1))}…"
       fi
     fi
-    sketchybar --set $NAME label="${TRACK}  ${ARTIST}" label.drawing=yes icon.color=0xffa6da95
+    sketchybar --set $NAME label="${ARTIST} ➟ ${TRACK}" label.drawing=yes icon.color=0xffa6da95
 
   elif [ $PLAYER_STATE = "Paused" ]; then
     sketchybar --set $NAME icon.color=0xffeed49f
