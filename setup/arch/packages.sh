2f07afb4f58003cf47fb9812214dbb8113644677
Dave Lens <github@davelens.be>
2025-09-13 12:05:25 +0200

    setup/arch: Add all packages I currently have installed to packages.sh.

--- setup/arch/packages.sh
+++ setup/arch/packages.sh
@@ -1,9 +1,67 @@
-# TODO: Fill in what's needed here
-sudo pacman -S -needed --noconfirm \
-  bash-completion \
+#!/usr/bin/env bash
+
+sudo pacman -S --needed \
+  base \
+  base-devel \
+  bash-language-server \
+  bitwarden \
   bitwarden-cli \
-  fastfetch \
+  cmake \
+  deno \
+  dkms \
+  dolphin \
+  dunst \
   fd \
+  firefox \
   fzf \
-  gcc14 \
-  github-cli
+  gdm \
+  git \
+  github-cli \
+  grim \
+  grub \
+  htop \
+  imagemagick \
+  iwd \
+  less \
+  libva-nvidia-driver \
+  linux \
+  linux-firmware \
+  linux-headers \
+  lolcat \
+  luarocks \
+  mariadb \
+  mesa-utils \
+  meson \
+  nano \
+  neovim \
+  nvidia-open-dkms \
+  openssh \
+  polkit-kde-agent \
+  qt5-wayland \
+  qt6-wayland \
+  rust \
+  shfmt \
+  slurp \
+  smartmontools \
+  starship \
+  tmux \
+  uwsm \
+  vim \
+  virtualbox-guest-utils \
+  wezterm \
+  wget \
+  wireless_tools \
+  wl-clipboard \
+  wofi \
+  wpa_supplicant \
+  xdg-utils \
+  xorg-server \
+  xorg-xinit \
+  zram-generator
+
+yay -S --needed \
+  asdf-vm \
+  getnf \
+  kanata \
+  yay \
+  yay-debug
