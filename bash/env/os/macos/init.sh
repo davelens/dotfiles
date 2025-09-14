a0a7362068911fbec86cadb8380e0b0cd77f9af2
davelens <github@davelens.be>
2025-09-14 17:20:35 +0200

    Remove the iterm2 shell integration, since I've been on WezTerm for a while now.

--- bash/env/os/macos/init.sh
+++ bash/env/os/macos/init.sh
@@ -1,9 +1,3 @@
-# Download the iTerm2 shell integration file and place it in the completions dir.
-if [ ! -f "$DOTFILES_REPO_HOME/bash/env/completions/iterm2_shell_integration.bash" ]; then
-  curl -L https://iterm2.com/shell_integration/bash \
-    -o "$DOTFILES_REPO_HOME/bash/env/completions/iterm2_shell_integration.bash"
-fi
-
 ###############################################################################
 # Additional settings/overrides to maintain behaviour across my machines.
 ###############################################################################
