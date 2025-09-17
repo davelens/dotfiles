8727af095c4058003fde58ce5d9aa5973d43ff2d
davelens <github@davelens.be>
2025-09-17 22:03:21 +0200

    setup/remote: Clean up step 3 confirmation message.

--- setup/remote/install_dotfiles.sh
+++ setup/remote/install_dotfiles.sh
@@ -1,7 +1,7 @@
 install_dotfiles() {
   # TODO: Change the `dots` command to git init DOTFILES_REPO_HOME, if necessary.
 
-  prompt="Do you want to proceed with installing the dotfiles? "
+  prompt="Do you want to proceed with installing the dotfiles? [y/n] "
   read -n1 -r -p "$prompt" input
 
   case $input in
