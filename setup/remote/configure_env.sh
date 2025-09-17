f4e3f654082a08cbf8561fafc8b29d892c2540d3
davelens <github@davelens.be>
2025-09-17 22:01:02 +0200

    setup/remote: Add configure_env.sh

---
+++ setup/remote/configure_env.sh
@@ -0,0 +1,31 @@
+configure_env() {
+  echo "TODO: Ask for:"
+  echo "- Github username"
+  echo "- Github email"
+  echo "- Git signing key"
+  echo "- GitHub personal access token"
+  echo "TODO: Set ENV vars:"
+  echo "- REPO_NAMESPACE"
+  echo "- GITHUB_USERNAME"
+  echo "- GITHUB_EMAIL"
+  echo "- GITHUB_SIGNING_KEY"
+  echo "- GITHUB_PERSONAL_ACCESS_TOKEN"
+  echo "TODO: Ask for OWNER_NAME as well? Potentially useful for bitwarden-cli"
+  echo "TODO: Generate $XDG_CONFIG_HOME/git/config.env using setup/gitconfig.env.template"
+}
+
+echo
+echo "4. $(underline "CONFIGURE ENVIRONMENT")"
+echo
+
+save_cursor
+
+configure_env
+
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+  reset_prompt
+  echo "✓ $(fgreen "Environment configured")"
+else
+  fail "x $(fred "Something went wrong during step 4.")"
+fi
