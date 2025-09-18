d7186a6b9a68c4f1537f70182b32f6d8690713cd
davelens <github@davelens.be>
2025-09-18 16:32:50 +0200

    env/mish: Set PAGER to just `less`.

--- bash/env/misc.sh
+++ bash/env/misc.sh
@@ -16,7 +16,7 @@ export GH_EDITOR="bash -c '$EDITOR'"
 export CLICOLOR=1
 
 # PAGER is the path to the program used to list the contents of files through
-export PAGER='less --quit-if-one-screen --no-init --ignore-case --RAW-CONTROL-CHARS --quiet --dumb'
+export PAGER='less'
 
 # Erlang history settings to have a cmd history in iex sessions.
 export ERL_AFLAGS="-kernel shell_history enabled"
