0cc66d3581b24b7dddad145154ed9b30478d0378
davelens <github@davelens.be>
2025-10-12 20:04:39 +0200

    setup/remote: Add `jq` as a preflight dependency.

--- setup/remote/preflight.sh
+++ setup/remote/preflight.sh
@@ -5,3 +5,13 @@ if macos_needs_newer_bash; then
 
   brew install bash
 fi
+
+if ! command -v jq >/dev/null; then
+  if [ -f /etc/arch-release ]; then
+    sudo pacman -S --noconfirm jq
+  elif [ "$(uname)" = "Darwin" ]; then
+    brew install jq
+  else
+    sudo apt-get update && sudo apt-get install -y jq
+  fi
+fi
