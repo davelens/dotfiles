f4c254f004d557217f9891e7b3d7ebe006bde4d1
davelens <github@davelens.be>
2025-12-07 15:50:29 +0100

    bash/env/os/macos: Remove the `ipv6on` and `ipv6off` aliases since I haven't needed them in ages.

--- bash/env/os/macos/aliases.sh
+++ bash/env/os/macos/aliases.sh
@@ -3,15 +3,10 @@
 # "This app is damaged and can't be opened."
 # "This app was downloaded from the Internet. Are you sure you want to open it?"
 alias allow='xattr -d com.apple.quarantine'
+
 # Puts rm'ed files into macos's bin instead of permanently deleting them.
 alias rm='$(which trash) &>/dev/null && trash'
 
 # Flush the Directory Service cache on macos. Useful for forcing
 # hostname/user/groups changes to update.
 alias clearcache='sudo dscacheutil -flushcache'
-
-# Disable/Enable ipv6 on the Wi-Fi interface.
-# This is useful for when you're on a network that doesn't support ipv6, but
-# I also haven't used this in AGES now. Consider removing these.
-alias ipv6off='networksetup -setv6off Wi-Fi'
-alias ipv6on='networksetup -setv6automatic Wi-Fi'
