2850f5d992203ea9ee64ede994c5987e1bfc0f73
davelens <github@davelens.be>
2025-12-07 15:49:11 +0100

    bash/env/os/macos: Clarify aliases.

--- bash/env/os/macos/aliases.sh
+++ bash/env/os/macos/aliases.sh
@@ -1,5 +1,10 @@
+# Remove the macOS quarantine attribute from downloaded files to bypass
+# Gatekeeper warnings like:
+# "This app is damaged and can't be opened."
+# "This app was downloaded from the Internet. Are you sure you want to open it?"
 alias allow='xattr -d com.apple.quarantine'
-alias rm='$(which trash) &>/dev/null && trash' # Puts rm'ed files in the OS trash
+# Puts rm'ed files into macos's bin instead of permanently deleting them.
+alias rm='$(which trash) &>/dev/null && trash'
 
 # Flush the Directory Service cache on macos. Useful for forcing
 # hostname/user/groups changes to update.
