f15aabdf0ede5eea8a7323136fb080faf64806c5
davelens <github@davelens.be>
2025-12-20 14:27:52 +0100

    setup: Move `packages.sh` into `brew/init.sh`, move the default Brewfile to `brew/`.

--- setup/brew/init.sh
+++ setup/brew/init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+if ! command -v brew >/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 brew bundle --file="$DOTFILES_REPO_HOME"/setup/Brewfile.default
 
 if [ "$(os)" == 'macos' ]; then
