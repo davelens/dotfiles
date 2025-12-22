fbe16a483ac18bd18464ca441458e6af869bbc41
davelens <github@davelens.be>
2025-12-23 00:13:44 +0100

    setup/mise: We can't rely on mise being installed yet, so call on the binary directly.

--- setup/mise/init.sh
+++ setup/mise/init.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-if command_exists mise; then
+MISE_BIN="$HOME/.local/bin/mise"
+
+if [ -x "$MISE_BIN" ]; then
   echo "Mise is already installed."
 else
   curl https://mise.run | sh
 fi
 
-mise upgrade
+eval "$("$MISE_BIN" activate bash)"
+"$MISE_BIN" upgrade
