ab4ecefbfbf12882e1ab36181f2e577852a1c2ed
davelens <github@davelens.be>
2025-12-28 15:34:12 +0100

    bash/env: Fix bash completion on Arch.

--- bash/env/completion.sh
+++ bash/env/completion.sh
@@ -2,19 +2,27 @@
 # Env settings and configuration related to bash's completion.
 ###############################################################################
 
-# Load brew's bash_completion, if available.
+# Load system bash-completion (Arch, Debian, etc,...).
+if [[ -r /usr/share/bash-completion/bash_completion ]]; then
+  source /usr/share/bash-completion/bash_completion
+fi
+
+# Load Homebrew-installed completions (macos or Linuxbrew).
 if command -v brew >/dev/null; then
-  if [ -r "$BREW_PATH"/etc/profile.d/bash_completion.sh ]; then
+  if [[ -r "$BREW_PATH"/etc/profile.d/bash_completion.sh ]]; then
     source "$BREW_PATH"/etc/profile.d/bash_completion.sh
   fi
 
-  if [ -d "$BREW_PATH"/etc/bash_completion.d/ ]; then
+  if [[ -d "$BREW_PATH"/etc/bash_completion.d/ ]]; then
     for completion in "$BREW_PATH"/etc/bash_completion.d/*; do
-      [ -r "$completion" ] && source "$completion"
+      [[ -r "$completion" ]] && source "$completion"
     done
     unset completion
   fi
 fi
+
+# Enable completion for commands run via `sudo`.
+complete -cf sudo
 
 # Source all downloaded completion files.
 for file in "$DOTFILES_REPO_HOME"/bash/env/completions/*.bash; do
