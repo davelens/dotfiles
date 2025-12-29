32c1a8ac24a5f00bee32c1b7c555fac859e75bbe
davelens <github@davelens.be>
2025-12-30 00:23:22 +0100

    bash/env: Set MANPAGER to "$EDITOR +Man!" for a better reading experience.

--- bash/env/misc.sh
+++ bash/env/misc.sh
@@ -2,21 +2,24 @@
 # Env settings and exports I couldn't quite (yet) fit into other categories.
 ###############################################################################
 
-# dvim is what loads in my nvim config
+# dvim is what loads in my nvim config.
 # https://github.com/davelens/dotvim
 export EDITOR="dvim"
 
-# Stop checking shellmail for new messages
+# Stop checking shellmail for new messages.
 unset MAILCHECK
 
 # This makes it so `gh` will use a bash shell running my default editor.
 export GH_EDITOR="bash -c '$EDITOR'"
 
-# Make ls & grep pretty
+# Make ls & grep pretty.
 export CLICOLOR=1
 
-# PAGER is the path to the program used to list the contents of files through
+# PAGER is the path to the program used to list the contents of files through.
 export PAGER='less'
+
+# MANPAGER is the program used to open and browse manuals.
+export MANPAGER="$EDITOR +Man!"
 
 # Erlang history settings to have a cmd history in iex sessions.
 export ERL_AFLAGS="-kernel shell_history enabled"
