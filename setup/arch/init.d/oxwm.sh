3c56521ea364f4162105fefeb3200c95fc9b14ec
davelens <github@davelens.be>
2025-12-31 17:07:10 +0100

    setup/arch: Add missing --needed --noconfirm to pacman/paru calls for oxwm.

--- setup/arch/init.d/oxwm.sh
+++ setup/arch/init.d/oxwm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-sudo pacman -S libx11 libxft freetype2 fontconfig pkg-config xterm
-paru -S oxwm-git
+sudo pacman -S --needed --noconfirm libx11 libxft freetype2 fontconfig pkg-config xterm
+paru -S --needed --noconfirm oxwm-git
