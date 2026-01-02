c0e6010cc2b473ade3bc822dca1485a4f49b3749
davelens <github@davelens.be>
2026-01-02 16:48:56 +0100

    setup/gnome: Add settings to customize keymaps on Arch.

--- setup/gnome/settings.sh
+++ setup/gnome/settings.sh
@@ -7,4 +7,21 @@ echo "==> Configuring GNOME settings..."
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
+# Disable default keybindings that conflict with custom workflows
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]"
+gsettings set org.gnome.desktop.wm.keybindings close "[]"
+
+# Custom keybindings
+CUSTOM_KEYBINDINGS_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+  "['${CUSTOM_KEYBINDINGS_PATH}/custom0/']"
+
+# Albert launcher: Super+Space
+dconf write "${CUSTOM_KEYBINDINGS_PATH}/custom0/name" "'Open Albert'"
+dconf write "${CUSTOM_KEYBINDINGS_PATH}/custom0/command" "'albert toggle'"
+dconf write "${CUSTOM_KEYBINDINGS_PATH}/custom0/binding" "'<Super>space'"
+
 echo "GNOME settings configured."
