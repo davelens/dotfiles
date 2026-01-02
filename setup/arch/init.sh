dc69a0f74c411702681c5ead1c9d25a0ae8704e9
davelens <github@davelens.be>
2026-01-02 13:58:43 +0100

    setup/arch: Restructure how packages are bootstrapped, and add the `u arch bundle` command so I can reuse it.

--- setup/arch/init.sh
+++ setup/arch/init.sh
@@ -6,6 +6,8 @@ set -e
 "$DOTFILES_REPO_HOME/setup/mise/init.sh"
 "$DOTFILES_REPO_HOME/setup/cargo/init.sh"
 "$DOTFILES_REPO_HOME/setup/gnome/init.sh"
+
+utility arch bundle --update
 "$DOTFILES_REPO_HOME/setup/arch/init.d/kanata.sh"
 "$DOTFILES_REPO_HOME/setup/arch/init.d/albert.sh"
 "$DOTFILES_REPO_HOME/setup/arch/init.d/mariadb.sh"
