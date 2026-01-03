98746eac30b7a8161e6dfd06a9f46d8c1267b620
davelens <github@davelens.be>
2026-01-03 01:33:31 +0100

    setup/arch: Changed my mind; revert to albert-bin because regular albert takes ages to compile.

--- setup/arch/init.d/albert.sh
+++ setup/arch/init.d/albert.sh
@@ -5,7 +5,7 @@ ln -sf "$DOTFILES_REPO_HOME/config/arch/systemd/albert.service" \
   "$XDG_CONFIG_HOME/systemd/user/"
 
 # --mflags --skipinteg: Skip integrity check (upstream checksum sometimes outdated)
-paru -S --needed --noconfirm --mflags --skipinteg albert
+paru -S --needed --noconfirm --mflags --skipinteg albert-bin
 systemctl --user daemon-reload
 systemctl --user restart albert
 
