43301ffe5358ae2b694f892d7968cde69869781e
davelens <github@davelens.be>
2026-01-03 20:40:10 +0100

    bash/env: Fix broken pbcopy/pbpaste aliases on Linux (wayland needs wl-copy and wl-paste, basically).

--- bash/env/os/linux/init.sh
+++ bash/env/os/linux/init.sh
@@ -1,8 +1,15 @@
 # This file will only load on Linux
 
 alias gls='ls' # We already use GNU ls in bash/aliases.sh
-alias pbcopy="xclip -selection clipboard"
-alias pbpaste="xclip -selection clipboard -o"
+
+# Clipboard aliases (pbcopy/pbpaste for macOS muscle memory)
+if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+  alias pbcopy="wl-copy"
+  alias pbpaste="wl-paste"
+else
+  alias pbcopy="xclip -selection clipboard"
+  alias pbpaste="xclip -selection clipboard -o"
+fi
 
 if [ -n "$BREW_PATH" ]; then
   # The mimemagic gem requires this file, which is installed via a homebrew pkg
