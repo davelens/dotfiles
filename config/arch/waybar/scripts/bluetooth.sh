e3ba33d34ca1e81440689d7345177927bd77f129
davelens <github@davelens.be>
2026-01-15 17:20:56 +0100

    config/arch: Greatly clean up the waybar configuration; remove unused files from mechabar, make it less of a mess in general.

--- config/arch/waybar/scripts/bluetooth.sh
+++ config/arch/waybar/scripts/bluetooth.sh
@@ -11,137 +11,132 @@
 # Date:    August 19, 2025
 # License: MIT
 
-# shellcheck disable=SC1090
-colors=()
-source ~/.config/waybar/scripts/fzf-colorizer.sh &> /dev/null || true
-
 RED="\e[31m"
 RESET="\e[39m"
 
 TIMEOUT=10
 
 ensure-on() {
-	local state
-	state=$(bluetoothctl show | awk '/PowerState/ {print $2}')
+  local state
+  state=$(bluetoothctl show | awk '/PowerState/ {print $2}')
 
-	case $state in
-		"off") bluetoothctl power on > /dev/null ;;
-		"off-blocked")
-			rfkill unblock bluetooth
+  case $state in
+  "off") bluetoothctl power on >/dev/null ;;
+  "off-blocked")
+    rfkill unblock bluetooth
 
-			local i new_state
-			for ((i = 1; i <= TIMEOUT; i++)); do
-				printf "\rUnblocking Bluetooth... (%d/%d)" $i $TIMEOUT
+    local i new_state
+    for ((i = 1; i <= TIMEOUT; i++)); do
+      printf "\rUnblocking Bluetooth... (%d/%d)" $i $TIMEOUT
 
-				new_state=$(bluetoothctl show | awk '/PowerState/ {print $2}')
-				if [[ $new_state == "on" ]]; then
-					break
-				fi
+      new_state=$(bluetoothctl show | awk '/PowerState/ {print $2}')
+      if [[ $new_state == "on" ]]; then
+        break
+      fi
 
-				sleep 1
-			done
+      sleep 1
+    done
 
-			if [[ $new_state != "on" ]]; then
-				notify-send "Bluetooth" "Failed to unblock" -i "package-purge"
-				return 1
-			fi
-			;;
-		*) return 0 ;;
-	esac
+    if [[ $new_state != "on" ]]; then
+      notify-send "Bluetooth" "Failed to unblock" -i "package-purge"
+      return 1
+    fi
+    ;;
+  *) return 0 ;;
+  esac
 
-	notify-send "Bluetooth On" -i "network-bluetooth-activated" \
-		-h string:x-canonical-private-synchronous:bluetooth
+  notify-send "Bluetooth On" -i "network-bluetooth-activated" \
+    -h string:x-canonical-private-synchronous:bluetooth
 }
 
 get-device-list() {
-	bluetoothctl -t $TIMEOUT scan on > /dev/null &
+  bluetoothctl -t $TIMEOUT scan on >/dev/null &
 
-	local i num
-	for ((i = 1; i <= TIMEOUT; i++)); do
-		printf "\rScanning for devices... (%d/%d)\n" $i $TIMEOUT
-		printf "%bPress [q] to stop%b\n" "$RED" "$RESET"
+  local i num
+  for ((i = 1; i <= TIMEOUT; i++)); do
+    printf "\rScanning for devices... (%d/%d)\n" $i $TIMEOUT
+    printf "%bPress [q] to stop%b\n" "$RED" "$RESET"
 
-		num=$(bluetoothctl devices | grep -c "Device")
-		printf "\nDevices: %s" "$num"
-		printf "\e[0;0H"
+    num=$(bluetoothctl devices | grep -c "Device")
+    printf "\nDevices: %s" "$num"
+    printf "\e[0;0H"
 
-		read -rsn 1 -t 1
-		if [[ $REPLY == [Qq] ]]; then
-			break
-		fi
-	done
+    read -rsn 1 -t 1
+    if [[ $REPLY == [Qq] ]]; then
+      break
+    fi
+  done
 
-	printf "\n%bScanning stopped.%b\n\n" "$RED" "$RESET"
+  printf "\n%bScanning stopped.%b\n\n" "$RED" "$RESET"
 
-	list=$(bluetoothctl devices | sed "s/^Device //")
-	if [[ -z $list ]]; then
-		notify-send "Bluetooth" "No devices found" -i "package-broken"
-		return 1
-	fi
+  list=$(bluetoothctl devices | sed "s/^Device //")
+  if [[ -z $list ]]; then
+    notify-send "Bluetooth" "No devices found" -i "package-broken"
+    return 1
+  fi
 }
 
 select-device() {
-	local header
-	header=$(printf "%-17s %s" "Address" "Name")
+  local header
+  header=$(printf "%-17s %s" "Address" "Name")
 
-	local options=(
-		"--border=sharp"
-		"--border-label= Bluetooth Devices "
-		"--ghost=Search"
-		"--header=$header"
-		"--height=~100%"
-		"--highlight-line"
-		"--info=inline-right"
-		"--pointer="
-		"--reverse"
-		"${colors[@]}"
-	)
+  local options=(
+    "--border=sharp"
+    "--border-label= Bluetooth Devices "
+    "--ghost=Search"
+    "--header=$header"
+    "--height=~100%"
+    "--highlight-line"
+    "--info=inline-right"
+    "--pointer="
+    "--reverse"
+  )
 
-	address=$(fzf "${options[@]}" <<< "$list" | awk '{print $1}')
-	if [[ -z $address ]]; then
-		return 1
-	fi
+  address=$(fzf "${options[@]}" <<<"$list" | awk '{print $1}')
+  if [[ -z $address ]]; then
+    return 1
+  fi
 
-	local connected
-	connected=$(bluetoothctl info "$address" | awk '/Connected/ {print $2}')
+  local connected
+  connected=$(bluetoothctl info "$address" | awk '/Connected/ {print $2}')
 
-	if [[ $connected == "yes" ]]; then
-		notify-send "Bluetooth" "Already connected to this device" \
-			-i "package-install"
-		return 1
-	fi
+  if [[ $connected == "yes" ]]; then
+    notify-send "Bluetooth" "Already connected to this device" \
+      -i "package-install"
+    return 1
+  fi
 }
 
 pair-and-connect() {
-	local paired
-	paired=$(bluetoothctl info "$address" | awk '/Paired/ {print $2}')
+  local paired
+  paired=$(bluetoothctl info "$address" | awk '/Paired/ {print $2}')
 
-	if [[ $paired == "no" ]]; then
-		printf "Pairing..."
+  if [[ $paired == "no" ]]; then
+    printf "Pairing..."
 
-		if ! timeout $TIMEOUT bluetoothctl pair "$address" > /dev/null; then
-			notify-send "Bluetooth" "Failed to pair" -i "package-purge"
-			return 1
-		fi
-	fi
+    if ! timeout $TIMEOUT bluetoothctl pair "$address" >/dev/null; then
+      notify-send "Bluetooth" "Failed to pair" -i "package-purge"
+      return 1
+    fi
+  fi
 
-	printf "\nConnecting..."
+  printf "\nConnecting..."
 
-	if ! timeout $TIMEOUT bluetoothctl connect "$address" > /dev/null; then
-		notify-send "Bluetooth" "Failed to connect" -i "package-purge"
-		return 1
-	fi
+  if ! timeout $TIMEOUT bluetoothctl connect "$address" >/dev/null; then
+    notify-send "Bluetooth" "Failed to connect" -i "package-purge"
+    return 1
+  fi
 
-	notify-send "Bluetooth" "Successfully connected" -i "package-install"
+  notify-send "Bluetooth" "Successfully connected" -i "package-install"
 }
 
 main() {
-	printf "\e[?25l"
-	ensure-on || exit 1
-	get-device-list || exit 1
-	printf "\e[?25h"
-	select-device || exit 1
-	pair-and-connect || exit 1
+  printf "\e[?25l"
+  ensure-on || exit 1
+  get-device-list || exit 1
+  printf "\e[?25h"
+  select-device || exit 1
+  pair-and-connect || exit 1
 }
 
 main
--- config/arch/waybar/scripts/power-menu.sh
+++ config/arch/waybar/scripts/power-menu.sh
@@ -8,43 +8,38 @@
 # Date:    August 19, 2025
 # License: MIT
 
-# shellcheck disable=SC1090
-colors=()
-source ~/.config/waybar/scripts/fzf-colorizer.sh &> /dev/null || true
-
 main() {
-	local list=(
-		"Lock"
-		"Shutdown"
-		"Reboot"
-		"Logout"
-		"Hibernate"
-		"Suspend"
-	)
+  local list=(
+    "Lock"
+    "Shutdown"
+    "Reboot"
+    "Logout"
+    "Hibernate"
+    "Suspend"
+  )
 
-	local options=(
-		"--border=sharp"
-		"--border-label= Power Menu "
-		"--height=~100%"
-		"--highlight-line"
-		"--no-input"
-		"--pointer="
-		"--reverse"
-		"${colors[@]}"
-	)
+  local options=(
+    "--border=sharp"
+    "--border-label= Power Menu "
+    "--height=~100%"
+    "--highlight-line"
+    "--no-input"
+    "--pointer="
+    "--reverse"
+  )
 
-	local selected
-	selected=$(printf "%s\n" "${list[@]}" | fzf "${options[@]}")
+  local selected
+  selected=$(printf "%s\n" "${list[@]}" | fzf "${options[@]}")
 
-	case $selected in
-		"Lock") loginctl lock-session ;;
-		"Shutdown") systemctl poweroff ;;
-		"Reboot") systemctl reboot ;;
-		"Logout") loginctl terminate-session "$XDG_SESSION_ID" ;;
-		"Hibernate") systemctl hibernate ;;
-		"Suspend") systemctl suspend ;;
-		*) exit 1 ;;
-	esac
+  case $selected in
+  "Lock") loginctl lock-session ;;
+  "Shutdown") systemctl poweroff ;;
+  "Reboot") systemctl reboot ;;
+  "Logout") loginctl terminate-session "$XDG_SESSION_ID" ;;
+  "Hibernate") systemctl hibernate ;;
+  "Suspend") systemctl suspend ;;
+  *) exit 1 ;;
+  esac
 }
 
 main
