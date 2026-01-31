53e23136a43a1784e1853bff10b6343eff1e3875
davelens <github@davelens.be>
2026-01-31 13:13:17 +0100

    bash/env/linux: Add a rofi-test alias so I can get a quick feel for how the different themes and styles look like.

--- bash/env/os/linux/aliases.sh
+++ bash/env/os/linux/aliases.sh
@@ -1,5 +1,7 @@
 alias gls='ls' # We already use GNU ls in bash/aliases.sh
 alias cpuclock='cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq'
+# shellcheck disable=SC2154
+alias rofi-test='for i in $(seq 8); do for j in $(seq 15); do echo "$i - $j"; rofi-start --launcher type-$i --theme style-$j; done; done'
 
 # Clipboard aliases (pbcopy/pbpaste for macOS muscle memory)
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
