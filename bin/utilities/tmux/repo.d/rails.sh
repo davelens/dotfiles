8e887fa275027ca7408f8d98ffc0e8551cc5d91b
davelens <github@davelens.be>
2026-06-30 17:22:21 +0200

    feat(utilities/tmux): let the Rails bootstrap bind its server to "[::]" to support ipv4 and 6 in conjunction with docker on the same machine

--- bin/utilities/tmux/repo.d/rails.sh
+++ bin/utilities/tmux/repo.d/rails.sh
@@ -29,7 +29,7 @@ repl_command() {
 }
 
 server_command() {
-  echo "{ bin/spring server & } && bin/rails s && bin/spring stop"
+  echo "{ bin/spring server & } && bin/rails s -b '[::]' && bin/spring stop"
 }
 
 bootstrap() {
