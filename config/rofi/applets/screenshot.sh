61371b2af54b207f8039771a97c71251fcb81cc7
davelens <github@davelens.be>
2026-09-20 19:11:28 +0200

    style(config/rofi): drop the dir message from the screenshot applet

--- config/rofi/applets/screenshot.sh
+++ config/rofi/applets/screenshot.sh
@@ -13,9 +13,7 @@ export DOTFILES_REPO_HOME
 rofi_cmd() {
   "$XDG_BIN_HOME/rofi-start" --dmenu --theme applet \
     -theme-str 'listview {lines: 5;}' \
-    -p "Screenshot" \
-    -mesg "Screenshots: ${dir/#$HOME/\~}
-Recordings:  ${videos_dir/#$HOME/\~}"
+    -p "Screenshot"
 }
 
 pipe_options_to_rofi() {
@@ -73,7 +71,6 @@ take_screenshot_area() {
 main() {
   geometry=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.current_mode.width)x\(.current_mode.height)"')
   dir="$(xdg-user-dir PICTURES)/screenshots"
-  videos_dir="$(xdg-user-dir VIDEOS)/screencasts"
   file="$(date +%Y-%m-%d-%H-%M-%S)_${geometry}.png"
 
   option_1="󰹑 Screenshot Area"
